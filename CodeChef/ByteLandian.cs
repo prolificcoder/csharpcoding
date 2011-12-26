35af5f93dbd8b27180def79f1b515dae8c4a1b44
Satyajit Malugu <satya@malugu.com>
2011-12-26 10:31:57 -0800

    Code chef problems

---
+++ CodeChef/ByteLandian.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChef
+{
+    class ByteLandian
+    {
+        static Dictionary<long, long> curMax= new Dictionary<long,long>();
+        public static void Main()
+        {
+            string input=string.Empty;
+            
+            while ((input = System.Console.ReadLine()) != null)
+            {   
+                long inpVal = long.Parse(input);
+                if (inpVal < 0 || inpVal > 1000000000)
+                    return;
+                Console.WriteLine(bankExgVal(inpVal));              
+            }            
+        }
+        public static long bankExgVal(long inp)
+        {
+            if (curMax.ContainsKey(inp))
+                return curMax[inp];
+            else
+            {
+                long val = 0;
+                if (inp < 12)
+                    val = inp;
+                else
+                    val= Math.Max(inp, bankExgVal(inp / 2) + bankExgVal(inp / 3) + bankExgVal(inp / 4));
+                curMax.Add(inp, val);
+                return val;
+            }
+        }
+    }
+}
---
+++ CodeChef/PrimesRange.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChef
+{
+    class PrimesRange
+    {
+        Dictionary<int, int> Primes = new Dictionary<int, int>();
+        Dictionary<int, int[]> ranges = new Dictionary<int, int[]>();
+        public static void Main()
+        {           
+            PrimesRange pr = new PrimesRange();
+            string input = string.Empty;
+            StringBuilder sb = new StringBuilder();
+            int numRange=int.Parse(System.Console.ReadLine());
+            for(int i =0; i<numRange;i++)
+            {
+                input = System.Console.ReadLine();
+                string[] iArgs = input.Split(' ');
+                pr.ranges.Add(i, new int[] { int.Parse(iArgs[0]), int.Parse(iArgs[1]) });
+            }
+            DateTime dt = DateTime.Now;
+            for(int i =0; i<numRange;i++)
+            {
+                int min = pr.ranges[i][0];
+                int max= pr.ranges[i][1];
+
+                for (int j = min; j <= max; j++)
+                {
+                    if (pr.Primes.ContainsKey(j) || pr.IsPrime(j))
+                    {
+                        sb.AppendLine(j.ToString());
+                        if (!pr.Primes.ContainsKey(j))
+                        {
+                            pr.Primes.Add(j, j);
+                        }
+                    }
+                }
+                sb.AppendLine();
+            }
+            Console.Write(sb);
+            Console.WriteLine(dt +"time"+ System.DateTime.Now);
+        }
+        public bool IsPrime(int n)
+        {
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
